7ccce898ce6677cdc16ad4da46fe155b52126425
EDEN <EDBEK@YAHOO.COM>
2020-09-09 17:17:01 -0500

    Created functions for basic calculator features and created onClick function

--- src/App.js
+++ src/App.js
@@ -12,6 +12,51 @@ class App extends Component {
     };
   }
 
+  onClick = (button) => {
+    if (button === "=") {
+      this.calculate();
+    } else if (button === "C") {
+      this.reset();
+    } else if (button === "CE") {
+      this.backspace();
+    } else {
+      this.setState({
+        result: this.state.result + button,
+      });
+    }
+  };
+
+  calculate = () => {
+    var checkResult = "";
+    if (this.state.result.includes("--")) {
+      checkResult = this.state.result.replace("--", "+");
+    } else {
+      checkResult = this.state.result;
+    }
+
+    try {
+      this.setState({
+        result: (eval(this.state.result) || "") + "",
+      });
+    } catch (e) {
+      this.setState({
+        result: "error",
+      });
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: "",
+    });
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1),
+    });
+  };
+
   render() {
     return (
       <div>
